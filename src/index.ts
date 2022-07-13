ecaf837e1998d65105ccfac9dedec4e008068314
boapi <tobias.weyer1205@gmail.com>
2022-07-13 22:51:59 +0200

    add lesson runner

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,27 @@
 
 import { intersectionLesson } from "./intersection/intersection";
 import { interfaceLesson } from "./interface/interface";
+import { typesLesson } from "./types/types"
 
-console.log(intersectionLesson());
-console.log(interfaceLesson());
+// define function signature for lessons array
+type Lesson = () => String
+
+/**
+ * @name lessons
+ * @description array of functions which represents lessons
+ */
+const lessons: Lesson[] = [
+  interfaceLesson,
+  intersectionLesson,
+  typesLesson,
+]
+
+/**
+ * @description looping of lessons array and call each lesson
+ */
+lessons.forEach((lesson: Function, i: Number): void => {
+  console.log("=====================")
+  console.log(`Lesson: ${i}`)
+  console.log(lesson())
+  console.log("=====================")
+});
