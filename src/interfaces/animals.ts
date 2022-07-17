9bcac2b7b8558fc0f6113fb0a0c8d099372cf85d
boapi <tobias.weyer1205@gmail.com>
2022-07-17 22:42:42 +0200

    add animal interfaces

---
+++ src/interfaces/animals.ts
@@ -0,0 +1,42 @@
+/**
+ * animal interfaces
+ */
+
+/**
+ * @name Animal
+ * @property {string}     type    describes the type of the animal
+ * @property {number}     legs    amount of legs
+ */
+
+interface Animal {
+  type: "dog" | "cat";
+  legs: number;
+}
+
+/**
+ * @name Dog
+ * @property {string}   name    name of the dog
+ * @property {string}   breed   breed of the dog
+ * @property {function} bark    sound of the dog
+ */
+
+interface Dog extends Animal {
+  name: string;
+  breed: string;
+  bark: () => string;
+}
+
+/**
+ * @name Cat
+ * @property {string}   name    name of the cat
+ * @property {string}   breed   breed of the cat
+ * @property {function} meow    sound of the cat
+ */
+
+interface Cat extends Animal {
+  name: string;
+  breed: string;
+  meow: () => string;
+}
+
+export { Dog, Cat };
